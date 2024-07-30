3cc1ce929fd9d7bc0af11cc27c5e5b52643cf400
Niccolò Toccane <nic.toccane@wonderflow.ai>
2024-07-30 22:11:13 +0200

    setup view model

--- app/src/main/java/com/example/mystressfulldiary/data/AppViewModel.kt
+++ app/src/main/java/com/example/mystressfulldiary/data/AppViewModel.kt
@@ -1,80 +1,73 @@
 package com.example.mystressfulldiary.data
 
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.asLiveData
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toJavaLocalDate
 import kotlinx.datetime.toLocalDateTime
-import kotlinx.datetime.todayIn
 
 class AppViewModel(private val repository: AppRepository): ViewModel() {
-    private var _causes = MutableLiveData<List<StressCause>>();
+    private var _causes = MutableLiveData<List<StressCause>>(emptyList());
     val causes: LiveData<List<StressCause>> get() = _causes;
-    private var _entries = MutableLiveData<List<StressEntry>>();
+
+    private var _todaysEntries = MutableLiveData<List<StressEntry>>(emptyList());
+    val todaysEntries: LiveData<List<StressEntry>> get() = _todaysEntries;
+
+    private var _calendarEntriesAggregation = MutableLiveData<List<StressDay>>(emptyList());
+    val calendarEntriesAggregation: LiveData<List<StressDay>> get() = _calendarEntriesAggregation;
+
+    private var _entries = MutableLiveData<List<StressEntry>>(emptyList());
     val entries: LiveData<List<StressEntry>> get() = _entries;
 
     init {
         viewModelScope.launch {
-            val fetchedCauses = repository.getAllCauses()
-            var todayEntries = repository.getEntriesByDate(Clock.System.now().toLocalDateTime(TimeZone.UTC).date)
-            // Log.d("test", Clock.System.todayIn(TimeZone.currentSystemDefault()).toString())
-            // Log.d("test", Clock.System.todayIn(TimeZone.currentSystemDefault()).toJavaLocalDate().toString())
-            Log.d("test",todayEntries.size.toString())
+            val scope = CoroutineScope(Dispatchers.Default)
+            val currentMoment = Clock.System.now().toLocalDateTime(TimeZone.UTC);
+            val currentDate = LocalDate(currentMoment.date.year, currentMoment.date.month, currentMoment.date.dayOfMonth);
+
+            val fetchedCauses = repository.getAllCauses();
+            val fetchedEntries = repository.getAllEntries();
+
+            val todayEntries = fetchedEntries.filter { e -> e.date == currentDate }.toMutableList();
+
             if (fetchedCauses.isNotEmpty() && todayEntries.isEmpty()) {
-                todayEntries = fetchedCauses
-                    .map { cause -> StressEntry(cause.name, 0.0f) }
-                todayEntries.map() { entry -> async { repository.registerEntry(entry) } }
+                fetchedCauses.forEach { cause ->
+                    val entry = StressEntry(cause.name, 0, currentDate);
+                    todayEntries += entry;
+                    scope.launch {
+                        repository.registerEntry(entry)
+                    }
+                }
             }
 
-            _entries.value = todayEntries;
+            _todaysEntries.value = todayEntries;
             _causes.value = fetchedCauses;
+            _entries.value = fetchedEntries;
+            _calendarEntriesAggregation.value = repository.aggregateByDateAndSumIntensity();
+
         }
     }
-    /*
-
-    // Function to update the LiveData state
-    fun updateState(newValue: String) {
-        _causes.value = newValue
-    }*/
-
-    /*fun getAllCauses() {
-        // return repository.getAllCauses().asLiveData(viewModelScope.coroutineContext)
-        _causes.value = repository.getAllCauses().asLiveData()
-
-    }*/
     fun addCause(stressCause: StressCause) {
         viewModelScope.launch {
             repository.addCause(stressCause);
-            val newCauses = causes.value.orEmpty().toMutableList()
-            newCauses.add(stressCause)
+            val newCauses = causes.value.orEmpty().toMutableList();
+            newCauses.add(stressCause);
             _causes.value = newCauses;
         }
-    }
-    suspend fun deleteCause(stressCause: StressCause) {
-        repository.deleteCause(stressCause)
-    }
-    suspend fun registerEntry(stressEntry: StressEntry) {
-        repository.registerEntry(stressEntry)
     }
     fun updateEntry(stressEntry: StressEntry) {
         viewModelScope.launch {
             repository.updateEntry(stressEntry)
         }
     }
-    fun deleteEntry(stressEntry: StressEntry) {
-        viewModelScope.launch {
-            repository.deleteEntry(stressEntry)
-        }
+
+    suspend fun getDaysWithoutStressCause(cause: String): List<StressEntry> {
+        return repository.getDaysWithoutStressCause(cause);
     }
 }
